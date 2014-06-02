1851f7d6f48df7f467bba1c4e498ccb1f91ee09f
Razzi <razzi53@gmail.com>
2014-06-02 13:56:14 +0200

    transition to jekyll >:)

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var livereload = require('gulp-livereload');
 var tinylr = require('tiny-lr')();
 
 gulp.task('reload', function() {
-  gulp.src(['index.html', 'css/main.css'])
+  gulp.src(['_site/*.html', 'css/main.css'])
     .pipe(livereload(tinylr));
 });
 
 
 gulp.task('default', function() {
   tinylr.listen(35729);
-  gulp.watch(['index.html', 'css/main.css'], ['reload']);
+  gulp.watch(['_site/*.html', 'css/main.css'], ['reload']);
 });
